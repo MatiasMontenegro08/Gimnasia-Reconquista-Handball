b09aca336ca28ccff9733b80824d3ef96d931f29
Montenegro Matias <matihandball08@gmail.com>
2024-08-27 11:59:31 -0300

    Inicio - Estructura basica del proyecto

---
+++ README.md
@@ -0,0 +1,1 @@
+# Proyecto Gimnasia Handball Reconquista
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Iniciando</h1>
+    </>
+  )
+}
+
+export default App

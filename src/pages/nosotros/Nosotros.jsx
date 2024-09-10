243bb505e78574fadbea61b2c21af384ed6249ac
Montenegro Matias <matihandball08@gmail.com>
2024-09-10 14:19:46 -0300

    cambios page-nosotros / fotos

--- src/pages/nosotros/Nosotros.jsx
+++ src/pages/nosotros/Nosotros.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import imgNosotros from '../../../public/img/torneos/nosotros.png'
-import logo from '../../../public/img/logos/EscudoGimnasiaHandballReconquista.svg'
+import imgNosotros from '../../../public/img/torneos/equipofemenino.svg'
 import './nosotros.css'
 
 const Nosotros = () => {
@@ -11,7 +10,9 @@ const Nosotros = () => {
             </div>
             <div className="historia">
                 <p>¡Gracias por visitar nuestra página! Somos el equipo de handball "Gimnasia Reconquista", un grupo de apasionados por este deporte que se unieron para crear algo especial en nuestra ciudad. Nuestro equipo se fundó en el año 2023. Somos un grupo de amigos que solíamos jugar handball en un antiguo club. Decidimos dar un paso adelante y formar nuestro propio equipo, naciendo así Gimnasia Reconquista. En nuestro primer año de competición, logramos un destacado segundo puesto en la liga local, un reflejo del esfuerzo y la dedicación de cada uno de nuestros miembros.</p>
-                <h2>Misión y Visión</h2>
+                <div className="container-titulo">
+                    <h2>Misión y Visión</h2>
+                </div>
                 <h3>Misión</h3>
                 <p>Crecer como equipo en la liga local, mejorando continuamente nuestro nivel de juego y cohesión grupal.</p>
                 <h3>Visión</h3>
